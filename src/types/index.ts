8839a2d7db4d5e72086573ca6bc7391cfdf7e3f9
heamlk <mehdiabderrazak60@gmail.com>
2025-05-11 19:41:25 +0000

    chore(types): add components types

---
+++ src/types/index.ts
@@ -0,0 +1,40 @@
+export type ContainerPropsTypes<T extends React.ElementType> = {
+  as?: T;
+  className?: string;
+  children: React.ReactNode;
+} & React.ComponentPropsWithoutRef<T>;
+
+export interface SkiptToContentsPropsType {
+  classNames: string;
+}
+
+export type VisuallyHiddenPropsType = {
+  children: React.ReactNode;
+} & React.ComponentPropsWithoutRef<"span">;
+
+export type ListPropsType<Item, As extends React.ElementType> = {
+  items: Item[];
+  renderItem: (item: Item) => React.ReactNode;
+  as?: As;
+};
+
+export interface TitlePropsType extends React.HTMLAttributes<HTMLElement> {
+  level: keyof React.JSX.IntrinsicElements;
+  ariaLevel: number | undefined;
+  children: React.ReactNode;
+}
+
+export type InputsPropsType = {
+  id?: string;
+  label?: string;
+  type: "search" | "text" | "email";
+} & React.ComponentPropsWithoutRef<"input">;
+
+export type FormPropsType = React.ComponentPropsWithoutRef<"form"> & {
+  children: React.ReactNode;
+};
+
+export type CardPropsType = {
+  children: React.ReactNode;
+  classNames?: string;
+} & React.ComponentPropsWithoutRef<"article">;
