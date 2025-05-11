0767990164d70c82a9dd7379262988626eedcfe4
heamlk <mehdiabderrazak60@gmail.com>
2025-05-11 19:42:07 +0000

    chore(constants): add the footer contents constants

---
+++ src/constants/index.ts
@@ -0,0 +1,112 @@
+const footerDescription =
+  "Best information about the best products and services.";
+
+const footerSocials = [
+  {
+    id: crypto.randomUUID(),
+    name: "Facebook",
+    link: "https://www.facebook.com/",
+    icon: "facebook",
+    title: "Facebook",
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Twitter",
+    link: "https://twitter.com/",
+    icon: "twitter",
+    title: "Twitter",
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "LinkedIn",
+    link: "https://www.linkedin.com/",
+    icon: "linkedin",
+    title: "LinkedIn",
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Instagram",
+    link: "https://www.instagram.com/",
+    icon: "instagram",
+    title: "Instagram",
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "YouTube",
+    link: "https://www.youtube.com/",
+    icon: "youtube",
+    title: "YouTube",
+  },
+];
+
+const footerLinksAbout = [
+  { id: crypto.randomUUID(), name: "About Us", link: "#", title: "About Us" },
+  {
+    id: crypto.randomUUID(),
+    name: "Find Store",
+    link: "#",
+    title: "Find Store",
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Categories",
+    link: "#",
+    title: "Categories",
+  },
+  { id: crypto.randomUUID(), name: "Blogs", link: "#", title: "Blogs" },
+];
+
+const footerLinksPartnership = [
+  { id: crypto.randomUUID(), name: "About Us", link: "#", title: "About Us" },
+  {
+    id: crypto.randomUUID(),
+    name: "Find Store",
+    link: "#",
+    title: "Find Store",
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Categories",
+    link: "#",
+    title: "Categories",
+  },
+  { id: crypto.randomUUID(), name: "Blogs", link: "#", title: "Blogs" },
+];
+
+const footerLinksInformation = [
+  {
+    id: crypto.randomUUID(),
+    name: "Help Center",
+    link: "#",
+    title: "Help Center",
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Money Refund",
+    link: "#",
+    title: "Money Refund",
+  },
+  { id: crypto.randomUUID(), name: "Shipping", link: "#", title: "Shipping" },
+  {
+    id: crypto.randomUUID(),
+    name: "Contact Us",
+    link: "#",
+    title: "Contact Us",
+  },
+];
+
+const footerLinksForUsers = [
+  { id: crypto.randomUUID(), name: "Login", link: "#", title: "Login" },
+  { id: crypto.randomUUID(), name: "Register", link: "#", title: "Register" },
+  { id: crypto.randomUUID(), name: "Settings", link: "#", title: "Settings" },
+  { id: crypto.randomUUID(), name: "My Orders", link: "#", title: "My Orders" },
+];
+
+export {
+  footerDescription,
+  footerSocials,
+  footerLinksAbout,
+  footerLinksPartnership,
+  footerLinksInformation,
+  footerLinksForUsers,
+};
