8d8074ce094c7c2bf8424d76173eb55a2f4ab0c4
heamlk <mehdiabderrazak60@gmail.com>
2025-05-13 15:07:36 +0000

    feat: add the homepage sections components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-import Container from "@/components/common/Container";
+import ExtraServices from "@/components/layouts/ExtraServices";
+import Hero from "@/components/layouts/Hero";
+import Newsletter from "@/components/layouts/Newsletter";
+import QuoteToSuppliers from "@/components/layouts/QuoteToSuppliers";
+import SingleNav from "@/components/layouts/SingleNav";
+import Suppliers from "@/components/layouts/Suppliers";
 
 export default function Home() {
   return (
-    <Container as={"main"} className="text-5xl  w-full h-screen">
-      Hello World
-    </Container>
+    <main className="bg-[#f7fafc]">
+      <SingleNav />
+      <Hero />
+      <QuoteToSuppliers />
+      <ExtraServices />
+      <Suppliers />
+      <Newsletter />
+    </main>
   );
 }
