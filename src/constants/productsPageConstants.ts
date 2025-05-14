7c09c339de1dd9066ab78556e35ef7dd7d48a23a
heamlk <mehdiabderrazak60@gmail.com>
2025-05-14 02:43:52 +0000

    chore(constants): update the constants file

---
+++ src/constants/productsPageConstants.ts
@@ -0,0 +1,79 @@
+import { ChevronRight } from "lucide-react";
+
+const productsBreadCrumbs = [
+  { id: crypto.randomUUID(), link: "home", icon: ChevronRight },
+  { id: crypto.randomUUID(), link: "clothings", icon: ChevronRight },
+  { id: crypto.randomUUID(), link: "men's wear", icon: ChevronRight },
+  { id: crypto.randomUUID(), link: "summer clothing", icon: ChevronRight },
+];
+
+const category = [
+  {
+    id: crypto.randomUUID(),
+    link: "mobile accessory",
+    linkUrl: "#",
+  },
+  {
+    id: crypto.randomUUID(),
+    link: "electronics",
+    linkUrl: "#",
+  },
+  {
+    id: crypto.randomUUID(),
+    link: "smartphones",
+    linkUrl: "#",
+  },
+  {
+    id: crypto.randomUUID(),
+    link: "modern techs",
+    linkUrl: "#",
+  },
+];
+
+const brands = [
+  {
+    id: crypto.randomUUID(),
+    link: "samsung",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "apple",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "huawei",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "pocco",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "lenovo",
+  },
+];
+
+const features = [
+  {
+    id: crypto.randomUUID(),
+    link: "metallic",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "plastic cover",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "8GB RAM",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "super power",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "large memory",
+  },
+];
+
+export { productsBreadCrumbs, category, brands, features };
