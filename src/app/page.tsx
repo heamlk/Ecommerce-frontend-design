c4355942c6b11479f68e1d7a2037c34781d56cd9
heamlk <mehdiabderrazak60@gmail.com>
2025-05-14 00:46:38 +0000

    feat: finish the homepage component and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import Consumers from "@/components/layouts/Consumers";
+import DealsAndOffers from "@/components/layouts/DealsAndOffers";
 import ExtraServices from "@/components/layouts/ExtraServices";
 import Hero from "@/components/layouts/Hero";
 import Newsletter from "@/components/layouts/Newsletter";
+import Outdoors from "@/components/layouts/Outdoors";
 import QuoteToSuppliers from "@/components/layouts/QuoteToSuppliers";
+import RecommendedItems from "@/components/layouts/RecommendedItems";
 import SingleNav from "@/components/layouts/SingleNav";
 import Suppliers from "@/components/layouts/Suppliers";
 
@@ -10,7 +14,11 @@ export default function Home() {
     <main className="bg-[#f7fafc]">
       <SingleNav />
       <Hero />
+      <DealsAndOffers />
+      <Outdoors />
+      <Consumers />
       <QuoteToSuppliers />
+      <RecommendedItems />
       <ExtraServices />
       <Suppliers />
       <Newsletter />
