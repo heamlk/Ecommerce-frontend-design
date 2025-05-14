9e6bf3d3ed6aca7ffabde416140467885ede4d08
heamlk <mehdiabderrazak60@gmail.com>
2025-05-14 03:15:23 +0000

    feat: add the products listing page

---
+++ src/app/products/page.tsx
@@ -0,0 +1,23 @@
+import Container from "@/components/common/Container";
+import Newsletter from "@/components/layouts/Newsletter";
+import ProductsBreadCrumbs from "@/components/layouts/ProductsBreadCrumbs";
+import ProductsList from "@/components/layouts/ProductsList";
+import ProductsSideBar from "@/components/layouts/ProductsSideBar";
+import SingleNav from "@/components/layouts/SingleNav";
+import { useProducts } from "@/hooks/useProducts";
+
+const page = () => {
+  return (
+    <main className="bg-[#f7fafc]">
+      <SingleNav />
+      <ProductsBreadCrumbs />
+      <Container as={"section"} className="grid grid-cols-4">
+        <ProductsSideBar />
+        <ProductsList />
+      </Container>
+      <Newsletter />
+    </main>
+  );
+};
+
+export default page;
