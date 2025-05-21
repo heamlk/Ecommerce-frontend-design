d608268dd1a49aa67bf45e909f1c8cb350816f27
heamlk <mehdiabderrazak60@gmail.com>
2025-05-21 23:51:50 +0000

    feat: update the product sidebar component

--- src/components/layouts/ProductsSideBar.tsx
+++ src/components/layouts/ProductsSideBar.tsx
@@ -1,17 +1,20 @@
-import { ChevronDownIcon, ChevronUpIcon } from "lucide-react";
+import { StarIcon } from "lucide-react";
+
 import List from "../common/List";
-import { brands, category, features } from "@/constants/productsPageConstants";
+import Accordion from "./Accordion";
+import Inputs from "../common/Input";
+
+import {
+  brands,
+  category,
+  conditions,
+  features,
+} from "@/constants/productsPageConstants";
 
 const ProductsSideBar = () => {
   return (
     <aside className="col-span-1 pr-6">
-      <div className="mb-5">
-        <div className="flex justify-between items-center border-t border-shade-200 py-4">
-          <p className="font-bold">Category</p>
-
-          <ChevronUpIcon />
-        </div>
-
+      <Accordion title="Category">
         <List
           items={category}
           className="flex flex-col gap-2"
@@ -29,15 +32,9 @@ const ProductsSideBar = () => {
         <a href="#" className="text-blue-900 pt-2 inline-block">
           See All
         </a>
-      </div>
+      </Accordion>
 
-      <div className="mb-5">
-        <div className="flex justify-between items-center border-t border-shade-200 py-4">
-          <p className="font-bold">Brands</p>
-
-          <ChevronUpIcon />
-        </div>
-
+      <Accordion title="Brands">
         <List
           items={brands}
           className="flex flex-col gap-2"
@@ -55,15 +52,9 @@ const ProductsSideBar = () => {
         <a href="#" className="text-blue-900 pt-2 inline-block">
           See All
         </a>
-      </div>
+      </Accordion>
 
-      <div className="mb-5">
-        <div className="flex justify-between items-center border-t border-shade-200 py-4">
-          <p className="font-bold">Features</p>
-
-          <ChevronUpIcon />
-        </div>
-
+      <Accordion title="Features">
         <List
           items={features}
           className="flex flex-col gap-2"
@@ -71,7 +62,6 @@ const ProductsSideBar = () => {
             return (
               <li key={item.id} className="flex items-center gap-3">
                 <input type="checkbox" name="checkbox" id="checkbox" />
-
                 <p className="text-shade-800 capitalize">{item.title}</p>
               </li>
             );
@@ -81,39 +71,95 @@ const ProductsSideBar = () => {
         <a href="#" className="text-blue-900 pt-2 inline-block">
           See All
         </a>
-      </div>
+      </Accordion>
 
-      <div>
-        <div className="flex justify-between items-center border-t border-shade-200 py-4">
-          <p className="font-bold">Price Range</p>
+      <Accordion title="Price Range">
+        <input type="range" name="range" id="range" className="w-full mb-3" />
 
-          <ChevronDownIcon />
+        <div className="flex items-center gap-3">
+          <Inputs
+            type="text"
+            placeholder="0"
+            label="Min"
+            className="bg-shade-100 border border-shade-200 rounded-md shadow-md px-2 py-1 w-full"
+          />
+
+          <Inputs
+            type="text"
+            placeholder="9999"
+            label="Max"
+            className="bg-shade-100 border border-shade-200 shadow-md rounded-md px-2 py-1 w-full"
+          />
         </div>
-      </div>
 
-      <div>
-        <div className="flex justify-between items-center border-t border-shade-200 py-4">
-          <p className="font-bold">Conditions</p>
+        <button
+          type="button"
+          className="w-full py-2 rounded-md border border-shade-200 bg-shade-100 shadow-md mt-4"
+        >
+          <span className="text-blue-900">Apply</span>
+        </button>
+      </Accordion>
 
-          <ChevronDownIcon />
+      <Accordion title="Condition">
+        <List
+          items={conditions}
+          className="flex flex-col gap-2"
+          renderItem={(item) => {
+            return (
+              <li key={item.id} className="flex items-center gap-3">
+                <input type="radio" name="radio" id="radio" />
+                <p className="text-shade-800 capitalize">{item.title}</p>
+              </li>
+            );
+          }}
+        />
+      </Accordion>
+
+      <Accordion title="Ratings">
+        <div className="flex flex-col gap-2">
+          <div className="flex items-center gap-2">
+            <input type="checkbox" name="checkbox" id="checkbox" />
+            <div className="flex items-center gap-1">
+              {[...Array(5)].map((_, index) => (
+                <StarIcon
+                  key={index}
+                  className="w-5 h-5"
+                  stroke="hsl(37, 77%, 58%)"
+                  fill="hsl(37, 77%, 58%)"
+                />
+              ))}
+            </div>
+          </div>
+
+          <div className="flex items-center gap-2">
+            <input type="checkbox" name="checkbox" id="checkbox" />
+            <div className="flex items-center gap-1">
+              {[...Array(4)].map((_, index) => (
+                <StarIcon
+                  key={index}
+                  className="w-5 h-5"
+                  stroke="hsl(37, 77%, 58%)"
+                  fill="hsl(37, 77%, 58%)"
+                />
+              ))}
+            </div>
+          </div>
+
+          <div className="flex items-center gap-2">
+            <input type="checkbox" name="checkbox" id="checkbox" />
+            <div className="flex items-center gap-1">
+              {[...Array(3)].map((_, index) => (
+                <StarIcon
+                  key={index}
+                  className="w-5 h-5"
+                  stroke="hsl(37, 77%, 58%)"
+                  fill="hsl(37, 77%, 58%)"
+                />
+              ))}
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div>
-        <div className="flex justify-between items-center border-t border-shade-200 py-4">
-          <p className="font-bold">Ratings</p>
-
-          <ChevronDownIcon />
-        </div>
-      </div>
-
-      <div>
-        <div className="flex justify-between items-center border-t border-shade-200 py-4">
-          <p className="font-bold">Manufacturers</p>
-
-          <ChevronDownIcon />
-        </div>
-      </div>
+      </Accordion>
     </aside>
   );
 };
